e74a17c318f373455f9fef640231371358905dfc
Vitalsonchik123 <Vitaliir2006@rambler.ru>
2026-05-20 23:42:21 +0300

    push lab_6

--- lab3-simple-app/components/header/index.js
+++ lab3-simple-app/components/header/index.js
@@ -15,7 +15,7 @@ export class HeaderComponent {
                                 <button id="home-button" class="btn btn-outline-light"> Домой</button>
                             </li>
                             <li class="nav-item">
-                                <button id="orders-button" class="btn btn-outline-light"> Заказы комплектующих</button>
+                                <button id="orders-button" class="btn btn-outline-light"> Датацентр. Комплектующие ПК</button>
                             </li>
                         </ul>
                     </div>
