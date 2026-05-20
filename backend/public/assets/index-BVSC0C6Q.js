c041bed92ba945dd34ca2773ddd65ffc264d6b7c
Vitalsonchik123 <Vitaliir2006@rambler.ru>
2026-05-21 00:30:49 +0300

    push lab_4

--- backend/public/assets/index-BVSC0C6Q.js
+++ backend/public/assets/index-BVSC0C6Q.js
@@ -1,14 +1,14 @@
 (function(){let e=document.createElement(`link`).relList;if(e&&e.supports&&e.supports(`modulepreload`))return;for(let e of document.querySelectorAll(`link[rel="modulepreload"]`))n(e);new MutationObserver(e=>{for(let t of e)if(t.type===`childList`)for(let e of t.addedNodes)e.tagName===`LINK`&&e.rel===`modulepreload`&&n(e)}).observe(document,{childList:!0,subtree:!0});function t(e){let t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin===`use-credentials`?t.credentials=`include`:e.crossOrigin===`anonymous`?t.credentials=`omit`:t.credentials=`same-origin`,t}function n(e){if(e.ep)return;e.ep=!0;let n=t(e);fetch(e.href,n)}})();var e=class{constructor(e){this.parent=e}getHTML(){return`
             <nav class="navbar navbar-expand-lg bg-success mb-4">
                 <div class="container-fluid">
-                    <a class="navbar-brand text-white" href="#"> Маркетплейс акций</a>
+                    <a class="navbar-brand text-white" href="#"> Датацентр. Комплектующие ПК</a>
                     <div class="collapse navbar-collapse">
                         <ul class="navbar-nav ms-auto">
                             <li class="nav-item me-2">
                                 <button id="home-button" class="btn btn-outline-light"> Домой</button>
                             </li>
                             <li class="nav-item">
-                                <button id="orders-button" class="btn btn-outline-light"> Датацентр. Комплектующие ПК</button>
+                                <button id="orders-button" class="btn btn-outline-light"> Заказы комплектующих</button>
                             </li>
                         </ul>
                     </div>
